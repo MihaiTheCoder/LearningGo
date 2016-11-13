a80da40da2c36e449dec15c3229e4873eb1a0cf4
Petrutiu Mihai <mihaipetrutiu@yahoo.com>
2016-11-13 16:03:48 +0200

    Update Collections

--- CollectionsProj/Collections.go
+++ CollectionsProj/Collections.go
@@ -2,33 +2,44 @@ package main
 
 import "fmt"
 
-func main()  {
-    myArray := [3]int{}
-    myArray[0] = 42
-    myArray[1] = 27
-    myArray[2] = 99
+func main() {
+	myArray := [3]int{} //Array of size 3
+	myArray[0] = 42
+	myArray[1] = 27
+	myArray[2] = 99
 
-    fmt.Println(myArray)
+	fmt.Println(myArray)
 
-    mySArray := [...]int{42, 77, 99}
-    fmt.Println(mySArray)
-    fmt.Println(len(mySArray))
+	mySArray := [...]int{42, 77, 99} //Use array iniliazer to set the initial values, size is replaced with ... because we have the values
+	fmt.Println(mySArray)
+	fmt.Println(len(mySArray)) //len(array) -> Get the size/length of any collection
 
-    mySlice := myArray[:]
-    fmt.Println(mySlice)
+	mySlice := myArray[:] //Put the entire array in a slice
+	fmt.Println(mySlice)
 
-    mySlice = append(mySlice, 100);
+	mySlice = append(mySlice, 100) //append to slice
 
-    fmt.Println(mySlice)
+	fmt.Println(mySlice)
 
-    mySSlice := []float32{12.,15.,18.}
-    fmt.Println(mySSlice)
+	a := [5]int{1, 2, 3, 4, 5} //array
+	s := a[1:4]                //slice with values 2,3,4
+	s1 := a[2:]                // same as a[2 : len(a)] ->3,4,5
+	s2 := a[:3]                // same as a[0 : 3] ->1,2,3
+	fmt.Println(s)
+	fmt.Println(s1)
+	fmt.Println(s2)
 
-    myMap := make(map[int]string)
-    fmt.Println(myMap)
-    myMap[42] = "Foo"
-    myMap[12] = "Bar"
+	mySSlice := []float32{12., 15., 18.} //Declaring a slice (because we don't have the size, or the ...)
+	fmt.Println(mySSlice)
 
-    fmt.Println(myMap)
-    fmt.Println(myMap[10])
+	myOSlice := make([]float32, 100) //create a slice with an initial size of 100
+	fmt.Println(myOSlice)
+
+	myMap := make(map[int]string) //map(dictionary) -> key of type int, value of type string
+	fmt.Println(myMap)
+	myMap[42] = "Foo"
+	myMap[12] = "Bar"
+
+	fmt.Println(myMap)
+	fmt.Println(myMap[10]) //Get[missingKey] ->Return default of string -> empty string
 }
