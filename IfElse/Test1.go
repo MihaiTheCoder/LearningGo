7a544843ccee432a1b9a8015e4c1688d7ed8c3a4
Petrutiu Mihai <mihaipetrutiu@yahoo.com>
2016-11-13 16:17:00 +0200

    Update branching

--- IfElse/Test1.go
+++ IfElse/Test1.go
@@ -1,35 +1,37 @@
 package main
 
 import (
-    "strconv"
+	"strconv"
 )
 
-func main()  {
-    foo := 2
+func main() {
+	foo := 2
 
-    if foo == 1 {
-        println("bar")
-    } else {
-        println("buz")
-    }
+	if foo == 1 {
+		println("bar")
+	} else if foo == 2 {
+		println("buz")
+	} else {
+		println("el patron")
+	}
 
-    if foo2 := 2; foo2 ==1 {
-     println("foo2 is 1")   
-    } else {
-        println("foo2 is not 1")
-    }
+	if foo2 := 2; foo2 == 1 {
+		println("foo2 is 1")
+	} else {
+		println("foo2 is not 1")
+	}
 
-    switch foo3 :=1; foo {
-    case 1: 
-        println(strconv.Itoa(foo3))        
-    }
+	switch foo3 := 1; foo {
+	case 1:
+		println(strconv.Itoa(foo3))
+	}
 
-    foo4 :=1
+	foo4 := 1
 
-    switch {
-    case foo4 == 1:
-        println("one")
-    case foo > 2:
-        println("two")
-    }
+	switch {
+	case foo4 == 1:
+		println("one")
+	case foo > 2:
+		println("two")
+	}
 }
