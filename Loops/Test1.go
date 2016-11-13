62dfac41a6584b6f33fd6f1327c35d504692c685
Petrutiu Mihai <mihaipetrutiu@yahoo.com>
2016-11-13 16:26:45 +0200

    Loops

--- Loops/Test1.go
+++ Loops/Test1.go
@@ -1,30 +1,30 @@
 package main
 
-func main()  {
-    for i := 1; i < 5; i++ {
-        println(i)
-    }
+func main() {
+	for i := 1; i < 5; i++ { //classic
+		println(i)
+	}
 
-    i := 0
-    for{
-        i++
-        println(i)
-        if(i >= 5) {
-            break
-        }
-    }
+	i := 0
+	for { //infinite for loop
+		i++
+		println(i)
+		if i >= 5 { //break the infinite -> Get out of loop
+			break
+		}
+	}
 
-    s := []string{"foo", "bar", "buz"}
-    for idx, v:= range s {
-        println(idx, v)
-    }
+	s := []string{"foo", "bar", "buz"} //slice
+	for idx, v := range s {            //Foreach: range returns 2 results, index/value
+		println(idx, v)
+	}
 
-    m := make(map[string]string)
-    m["first"] = "foo"
-    m["second"] ="bar"
-    m["third"] = "buz"
+	m := make(map[string]string)
+	m["first"] = "foo"
+	m["second"] = "bar"
+	m["third"] = "buz"
 
-    for k, v := range m {
-        println(k, v)
-    }
+	for k, v := range m { //Foreach over map, range returns key/value pair
+		println(k, v)
+	}
 }
