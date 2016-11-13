93c189732a92a9a7c19d0d7106551934d08b059a
Petrutiu Mihai <mihaipetrutiu@yahoo.com>
2016-11-13 16:21:26 +0200

    Update Branching

--- IfElse/Test1.go
+++ IfElse/Test1.go
@@ -15,20 +15,20 @@ func main() {
 		println("el patron")
 	}
 
-	if foo2 := 2; foo2 == 1 {
+	if foo2 := 2; foo2 == 1 { // you can declare variable inside of if
 		println("foo2 is 1")
 	} else {
 		println("foo2 is not 1")
 	}
 
-	switch foo3 := 1; foo {
+	switch foo3 := 1; foo { //you can declare variable inside of switch statement
 	case 1:
 		println(strconv.Itoa(foo3))
 	}
 
 	foo4 := 1
 
-	switch {
+	switch { //You can make a switch where every condition might use a different condition with different variables
 	case foo4 == 1:
 		println("one")
 	case foo > 2:
